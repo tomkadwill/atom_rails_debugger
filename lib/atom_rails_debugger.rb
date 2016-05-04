8d9cb406ace25fffa935d154a5664cf3d7f4fd62
Tom Kadwill <tomkadwill@gmail.com>
2016-05-04 21:30:04 +0100

    Remove hardcoded path to breakpoints file

--- lib/atom_rails_debugger.rb
+++ lib/atom_rails_debugger.rb
@@ -2,7 +2,7 @@ require "atom_rails_debugger/version"
 
 module AtomRailsDebugger
   def self.included(base)
-    File.open("/Users/thomaskadwill/Workspace/test-ruby-debug/breakpoints.txt", "r").each_line do |file_line|
+    File.open(Rails.root.join('breakpoints.txt'), "r").each_line do |file_line|
       file, line = file_line.strip.split(":")
       Byebug::Breakpoint.add(file, line.to_i)
     end
