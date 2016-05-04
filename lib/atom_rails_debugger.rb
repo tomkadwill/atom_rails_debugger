25a7cdb172773d87425f4026939e4c2e88e16355
Tom Kadwill <tomkadwill@gmail.com>
2016-05-04 22:00:12 +0100

    Evaluate breakpoint file on each request

--- lib/atom_rails_debugger.rb
+++ lib/atom_rails_debugger.rb
@@ -2,7 +2,16 @@ require "atom_rails_debugger/version"
 
 module AtomRailsDebugger
   def self.included(base)
-    File.open(Rails.root.join('breakpoints.txt'), "r").each_line do |file_line|
+    base.class_eval do
+      before_filter :eval_breakpoints
+    end
+  end
+
+  def eval_breakpoints
+    # Remove all breakpoints before re-evaluating the breakpoints file
+    Byebug.breakpoints.reject! { true }
+
+    File.open(Rails.root.join('.breakpoints'), "r").each_line do |file_line|
       file, line = file_line.strip.split(":")
       Byebug::Breakpoint.add(file, line.to_i)
     end
